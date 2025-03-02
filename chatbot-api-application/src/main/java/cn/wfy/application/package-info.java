bfcc13e3461beb633b5617fecfc3fbf589d206c4
wangfuyang <1046636531@qq.com>
2025-03-02 14:33:53 +0800

    init

---
+++ chatbot-api-application/src/main/java/cn/wfy/application/package-info.java
@@ -0,0 +1,1 @@
+package cn.wfy.application;
---
+++ chatbot-api-interfaces/src/main/java/cn/wfy/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.wfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wfy
+ * @description 启动入口
+ * Hello world!
+ */
+@SpringBootApplication
+public class ApiApplication
+{
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
